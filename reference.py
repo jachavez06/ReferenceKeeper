a9b77c401ea956e02f98c9eeab40f56eec00d95a
Jesus Chavez <jachavez06@gmail.com>
2017-10-02 20:37:20 -0500

    str and repr added

--- reference.py
+++ reference.py
@@ -8,7 +8,8 @@ Created on Mon Oct  2 19:51:48 2017
 
 class Reference:
     """Represents a single entry"""
-
+    
+    # Constructor
     def __init__(self):
         # instance variables unique to each Reference instance
         self.source_type = 'Journal Article'
@@ -21,3 +22,10 @@ class Reference:
         self.pages_end = None
         self.file = None
     
+    # For call to repr(). Prints object's information
+    def __repr__(self):
+        return 'Reference(%s, %s)' % (self.article_title, self.journal_title)
+    
+    # For call to str(). Prints readable form
+    def __str__(self):
+        return 'Test'
