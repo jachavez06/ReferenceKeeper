4c6d8b27fb485fb3f472111133fc8ced4d8051b8
Jesus Chavez <jachavez06@gmail.com>
2017-10-02 20:03:42 -0500

    Add test file for Reference

--- reference.py
+++ reference.py
@@ -7,4 +7,9 @@ Created on Mon Oct  2 19:51:48 2017
 """
 
 class Reference:
-    """Represents a single entry""" 
+    """Represents a single entry"""
+    
+    kind = 'canine'         # class variable shared by all instances
+
+    def __init__(self, name):
+        self.name = name    # instance variable unique to each instance
---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 19:57:38 2017
+
+@author: jesse
+"""
+
+import reference
+
+d = reference.Reference('Fido')
