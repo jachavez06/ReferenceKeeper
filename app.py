b89803063492d3473c9493d59dc54c514fd69fbb
Jesus Chavez <jachavez06@gmail.com>
2017-10-02 19:54:45 -0500

    Create refernece class

--- app.py
+++ app.py
@@ -1,29 +1,62 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
-from PyQt4.QtGui import *
- 
-# Create an PyQT4 application object.
-a = QApplication(sys.argv)
- 
-# The QWidget widget is the base class of all user interface objects in PyQt4.
-w = QWidget()
+from PyQt4 import QtGui, QtCore
 
-# Set window size.
-w.resize(320, 240)
- 
-# Set window title
-w.setWindowTitle("Hello World!")
 
-# -----
-# Get filename using QFileDialog
-filename = QFileDialog.getOpenFileName(w, 'Open File', '/')
-print filename
- 
-# print file contents
-with open(filename, 'r') as f:
-    print(f.read())
-# -----
-# Show window
-w.show()
- 
-sys.exit(a.exec_())
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.initUI()
+
+    # Defaults for main app window
+    def initUI(self):               
+
+        self.statusBar().showMessage('Ready')
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+        
+        # Add layout
+        layout = QtGui.QVBoxLayout()
+        
+        # Button
+        self.button = QtGui.QPushButton('Test', self)
+        self.button.clicked.connect(self.close)
+        layout.addWidget(self.button)
+        
+        # Open File Button
+        self.ofile_button = QtGui.QPushButton('Open', self)
+        self.ofile_button.move(100,0)
+        self.ofile_button.clicked.connect(self.openFileDialog)
+        layout.addWidget(self.ofile_button)
+        
+        self.show()
+    
+    def openFileDialog():
+        filename = QFileDialog.getOpenFileName(w, 'Open File', '/')
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    main_window = MainWindow()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+    # -----
+    # Get filename using QFileDialog
+    #filename = QFileDialog.getOpenFileName(w, 'Open File', '/')
+
+    #if filename:
+    #    print filename
+        
+        # print file contents
+    #    with open(filename, 'r') as f:
+    #        print(f.read())
+    # -----
+
---
+++ reference.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 19:51:48 2017
+
+@author: jesse
+"""
+
+class Reference:
+    """Represents a single entry""" 
