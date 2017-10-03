f89245c2ef13c900945db8e6fe3bc69523352574
Jesus Chavez <jachavez06@gmail.com>
2017-10-02 20:50:00 -0500

    Thought about using attributes over properties. Don't think I will.

--- reference.py
+++ reference.py
@@ -11,10 +11,9 @@ class Reference:
     
     # Constructor
     def __init__(self):
-        # instance variables unique to each Reference instance
-        self.source_type = 'Journal Article'
-        self.article_title = ''
-        self.journal_title = ''
+        self.source_type = None
+        self.article_title = None
+        self.journal_title = None
         self.volume_number = None
         self.issue_number = None
         self.year_published = None
