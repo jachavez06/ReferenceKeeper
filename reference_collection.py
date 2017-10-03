4d19e58e348ff1855ee4c1b60f0db988e718167c
Jesus Chavez <jachavez06@gmail.com>
2017-10-03 08:26:10 -0500

    Created reference collection object

---
+++ reference_collection.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 08:14:21 2017
+
+@author: jesse
+"""
+
+class ReferenceCollection:
+    
+    def __init__(self):
+        self.references = []
+    
+    def add(self, reference):
+        self.references.append(reference)
